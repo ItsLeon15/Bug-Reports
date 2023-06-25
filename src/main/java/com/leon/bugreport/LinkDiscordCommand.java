e0f7cc065a863ebb6b21821f204ad3ec30628886
Leon Dawson-Couper <seanw8429@gmail.com>
2023-06-25 02:31:24 +0100

    Fixed bugs relating to TTS and permissions

--- src/main/java/com/leon/bugreport/LinkDiscordCommand.java
+++ src/main/java/com/leon/bugreport/LinkDiscordCommand.java
@@ -22,21 +22,25 @@ public class LinkDiscordCommand implements CommandExecutor {
 
         Player player = (Player) sender;
 
-        if (args.length < 1) {
-            player.sendMessage(ChatColor.RED + "Usage: /buglinkdiscord <webhook URL>");
-            return true;
+        if (player.hasPermission("bugreport.admin")) {
+            if (args.length < 1) {
+                player.sendMessage(ChatColor.RED + "Usage: /buglinkdiscord <webhook URL>");
+                return true;
+            }
+
+            String webhookURL = args[0];
+
+            if (!reportManager.isWebhookURLValid(webhookURL)) {
+                player.sendMessage(ChatColor.RED + "Invalid webhook URL.");
+                return true;
+            }
+
+            reportManager.setWebhookURL(webhookURL);
+
+            player.sendMessage(ChatColor.GREEN + "Webhook URL has been set successfully.");
+        } else {
+            player.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
         }
-
-        String webhookURL = args[0];
-
-        if (!reportManager.isWebhookURLValid(webhookURL)) {
-            player.sendMessage(ChatColor.RED + "Invalid webhook URL.");
-            return true;
-        }
-
-        reportManager.setWebhookURL(webhookURL);
-
-        player.sendMessage(ChatColor.GREEN + "Webhook URL has been set successfully.");
 
         return true;
     }
